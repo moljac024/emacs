d996ae8da05c009707b386b435e3d93c190dab13
Bojan Matić <matic.bojan@gmail.com>
2014-06-28 10:03:39 +0200

    [minor] Move comment

--- config/auto-complete.el
+++ config/auto-complete.el
@@ -10,7 +10,7 @@
 ;; No delay, show completion immediately
 (setq company-idle-delay t)
 
+;; Disable company mode in eshell
 (add-hook 'eshell-mode-hook
   (lambda ()
-    ;; Disable company mode
     (company-mode -1)))
